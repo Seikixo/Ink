988da96ff4c98338b96895ed7324c90cb80f9e22
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-26 21:15:01 +0800

    Services Layout

--- resources/views/artist/index.blade.php
+++ resources/views/artist/index.blade.php
@@ -2,4 +2,5 @@
     <x-header></x-header>
     <x-banner></x-banner>
     <x-artist></x-artist>
+    <x-services></x-services>
 </x-layout>
---
+++ resources/views/components/services.blade.php
@@ -0,0 +1,16 @@
+<div class="flex h-[50vh] flex-col relative mt-44 text-white">
+    <div class="flex flex-col w-full h-full absolute justify-center items-center gap-20">
+        <div class="flex flex-col items-center z-40 gap-10">
+            <p class="font-megrim text-4xl">Services;</p>
+            <p class="font-dosis">Harmonizing Art and Expression</p>
+        </div>
+
+        <div class="flex flex-row font-megrim z-40 gap-96 text-4xl ">
+            <button class="flex w-64 h-16 justify-center items-center hover:bg-custom-green">Tattoos</button>
+            <button class="flex w-64 h-16 justify-center items-center hover:bg-custom-green">Aesthetics</button>
+        </div>
+    </div>
+    <img class="absolute" src="{{ asset('images/bg-3.jpg')}}" alt="">
+
+
+</div>
