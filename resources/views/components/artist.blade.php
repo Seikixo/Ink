63d32fe5243d0e4446236e3690143bd23fd667a4
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-26 20:32:17 +0800

    Artists Layout

--- resources/views/components/artist.blade.php
+++ resources/views/components/artist.blade.php
@@ -8,14 +8,27 @@
             Crafting Dreams into Stunning Wearable Art
         </div>
     </div>
-    <div class="flex flex-col justify-center items-center min-h-screen shadow-2xl bg-custom-blue p-16">
-        <div class="grid grid-cols-3 grid-rows-2 w-full h-full px-32 py-24 gap-6">
+
+    <div class="flex flex-col justify-center items-center min-h-screen w-full py-10 shadow-[-1px_6px_39px_100px_#B1C2D6] bg-custom-blue">
+        <div class="grid grid-cols-3 grid-rows-2 w-full h-full px-40 py-32 gap-6">
             <img class="grayscale" src="{{asset('images/img-artist-1.png')}}" alt="">
-            <img src="{{asset('images/img-artist-2.png')}}" alt="">
+            <div class="relative flex">
+                <img src="{{asset('images/img-artist-2.png')}}" alt="">
+                <div class="absolute h-full flex flex-col justify-center bg-white/25 backdrop-blur-lg">
+                    <div class="w-24 -rotate-90 flex flex-col justify-center items-center ">
+                        <p class="font-megrim text-2xl">SOLANA MERCURE</p>
+                        <p class="font-dosis">Tattoo Artist</p>
+                    </div>
+                </div>
+            </div>
             <img class="grayscale" src="{{asset('images/img-artist-3.png')}}" alt="">
             <img class="grayscale" src="{{asset('images/img-artist-4.png')}}" alt="">
             <img class="grayscale" src="{{asset('images/img-artist-5.png')}}" alt="">
             <img class="grayscale" src="{{asset('images/img-artist-6.png')}}" alt="">
+        </div>
+
+        <div class="w-full flex justify-center bottom-5 p-2 tracking-widest font-semibold font-dosis bg-custom-green text-white">
+            <p>INQUIRE NOW</p>
         </div>
     </div>
 
